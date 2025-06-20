c1f652d09c749fa27422176a5645abd8c74d36c8
keygen_exe <matkovskiyf3vlad@gmail.com>
2025-06-20 03:39:49 +0300

    res

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const ul = document.querySelector('.tree');
+
+ul.addEventListener('click', (e) => {
+  const liTarget = e.target.closest('li');
+
+  if (!liTarget || e.target.tagName !== 'SPAN') {
+    return;
+  }
+
+  const ulTarget = liTarget.querySelector('ul');
+
+  if (!ulTarget) {
+    return;
+  }
+
+  const computedDisplay = getComputedStyle(ulTarget).display;
+
+  ulTarget.style.display = computedDisplay === 'none' ? 'block' : 'none';
+});
