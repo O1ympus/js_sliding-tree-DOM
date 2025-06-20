4121e5c99c094c95cadcdd44c760570b729a714d
keygen_exe <matkovskiyf3vlad@gmail.com>
2025-06-20 03:53:25 +0300

    res

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,23 @@
-'use strict';
-
 const ul = document.querySelector('.tree');
+
+[...ul.querySelectorAll('li')].forEach((li) => {
+  const textNodes = [...li.childNodes].filter(
+    (node) =>
+      node.nodeType === Node.TEXT_NODE && node.textContent.trim() !== '',
+  );
+
+  if (textNodes.length > 0) {
+    const span = document.createElement('span');
+
+    span.textContent = textNodes
+      .map((node) => node.textContent.trim())
+      .join(' ');
+
+    textNodes.forEach((node) => li.removeChild(node));
+
+    li.insertBefore(span, li.firstChild);
+  }
+});
 
 ul.addEventListener('click', (e) => {
   const liTarget = e.target.closest('li');
